c1d02e594a26545c896d5196eb9b09872598a468
Abhay khandal <abhaykhandal2111@gmail.com>
2023-08-14 17:06:47 +0530

    code committed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Register from './Components/Register';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import SiderBar from './Components/SiderBar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/Siderbar" element={<SiderBar />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
